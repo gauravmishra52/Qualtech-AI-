9e2e646818bde9b99958f0f54efd09395169c180
GAURAV MISHRA <156705921+gauravmishra52@users.noreply.github.com>
2025-12-15 15:21:15 +0530

    chore: add initial Spring Boot project (Java 21)

---
+++ src/main/java/com/qualtech/qualtech_ai/QualtechAiApplication.java
@@ -0,0 +1,13 @@
+package com.qualtech.qualtech_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualtechAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualtechAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qualtech/qualtech_ai/QualtechAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualtech.qualtech_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualtechAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
